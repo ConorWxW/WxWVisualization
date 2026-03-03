2a7377367f056c122e483adabd6a05d5edd31f7e
ofHillsAndLakes <conorohaleck@gmail.com>
2026-03-02 23:34:31 -0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Graph from 'graphology'
+import Sigma from 'sigma' 
+
+const graph = new Graph();
+graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
+graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
+graph.addEdge("1", "2", { size: 5, color: "purple" });
+
+const sigmaInstance = new Sigma(graph, document.getElementById("container"));
 
 export default function Home() {
   return (
@@ -15,6 +24,8 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <div id="container" style={{ width: "400px", height: "400px" }}></div>
+        
       </main>
 
       <Footer />
