3779731aa91ccb1cff616a02118ed0a41c9c422a
Conor O'Haleck <conor.ohaleck@whatbywhen.com>
2026-03-16 09:15:43 -0700

    added graph preview

--- pages/index.js
+++ pages/index.js
@@ -20,11 +20,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-        <div id="container" style={{ width: "400px", height: "400px" }}></div>
+        <div id="container" style={{ width: "400px", height: "400px" }}>
+          <script src="/graphy.js"></script>
+        </div>
         
       </main>
 
